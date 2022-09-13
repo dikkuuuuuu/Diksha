83856c9a6e608fb40d383171828b1f3440cf9b88
dikkuuuuuu <113029737+dikkuuuuuu@users.noreply.github.com>
2022-09-13 10:45:43 +0100

    myProject
    i have created one project

---
+++ DalalStreet/src/myProject/String.java
@@ -0,0 +1,26 @@
+package myProject;
+
+public class String 
+{
+	
+		 public void mess()
+		 {
+			 java.lang.String m= "bring";
+			 System.out.println(m);
+		 }
+		 public static void mean()
+		 {
+			 java.lang.String n="Diksha";
+			 System.out.println(n);
+		 }
+		 public static void main(String args[])
+		 {
+			  String obj = new String();
+			  obj.mess();
+			  String.mean();
+			 
+			 
+		 }
+		}
+
+
---
+++ JavaProject/src/myProject/String.java
@@ -0,0 +1,25 @@
+package myProject;
+
+public class String
+	{
+	 public void mess()
+	 {
+		 String A ="Nagpur";
+		 System.out.println(A);
+	 }
+	 public static void mean()
+	 {
+		 String B="Diksha";
+		 System.out.println(B);
+	 }
+	 public static void main(String args[])
+	 {
+		  STRING obj = new STRING();
+		  obj.test();
+		  STRING.mean();
+		 
+		 
+	 }
+	}
+
+}
